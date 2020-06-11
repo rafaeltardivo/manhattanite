e8f7f3a231c7819023519143d33c9270a82043f7
Rafael Tardivo <rafaeldt.br@gmail.com>
2020-06-11 20:56:00 -0300

    Add service initial blueprint

--- cartesian/service.go
+++ cartesian/service.go
@@ -1,1 +1,19 @@
 package cartesian
+
+type Point struct {
+	x int
+	y int
+}
+
+// Cartesian service interface definition
+type CartesianService interface {
+	findPointsWithinDistance(Point, int)
+}
+
+// Service implementation according to CartesianService
+type service struct{}
+
+// Finds and returns points within queried point origin and distance
+func (s *service) findPointsWithinDistance(queriedPoint Point, maxDistance int) ([]Point, error) {
+	return nil, nil
+}
