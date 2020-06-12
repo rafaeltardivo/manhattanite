bb0ec6467a7a73743c6dd8996552d9b8dbc5d880
Rafael Tardivo <rafaeldt.br@gmail.com>
2020-06-11 21:00:49 -0300

    Add cartesian repository initial blueprint

--- cartesian/repository.go
+++ cartesian/repository.go
@@ -1,1 +1,14 @@
 package cartesian
+
+// Cartesian repository interface definition
+type CartesianRepository interface {
+	loadPoints() []Point
+}
+
+// Repository implementation of CartesianRepository
+type repository struct{}
+
+// Loads and returns  predefined points
+func (r *repository) loadPoints() {
+
+}
