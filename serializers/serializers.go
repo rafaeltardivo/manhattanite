a16f7c8f060e96f4c3e1c1e4ae0955a2fa3ec22f
Rafael Tardivo <rafaeldt.br@gmail.com>
2020-06-13 00:02:28 -0300

    Add JSON serializer Decode implementation

--- serializers/serializers.go
+++ serializers/serializers.go
@@ -1,7 +1,25 @@
 package serializers
 
-import "github.com/manhattanite/services"
+import (
+	"encoding/json"
 
+	"github.com/manhattanite/services"
+)
+
+// Cartesian serializer interface definition
 type CartesianSerializer interface {
 	Decode(raw []byte) ([]services.Point, error)
+}
+
+// JSON implementation of cartesian serializer interface
+type JSONSerializer struct{}
+
+// Decodes raw byte array to an array of points
+func (j *JSONSerializer) Decode(raw []byte) ([]services.Point, error) {
+	var points []services.Point
+
+	if err := json.Unmarshal(raw, &points); err != nil {
+		return nil, ErrSerialize(err.Error())
+	}
+	return points, nil
 }
