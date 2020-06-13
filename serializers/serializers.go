9d78b9b9c465a1659e4a2e1d4114555167f3cb30
Rafael Tardivo <rafaeldt.br@gmail.com>
2020-06-13 20:03:32 -0300

    Update Point reference from serializers to models

--- serializers/serializers.go
+++ serializers/serializers.go
@@ -3,23 +3,28 @@ package serializers
 import (
 	"encoding/json"
 
-	"github.com/manhattanite/services"
+	"github.com/manhattanite/models"
 )
 
 // Cartesian serializer interface definition
 type CartesianSerializer interface {
-	Decode(raw []byte) ([]services.Point, error)
+	Decode(raw []byte) ([]models.Point, error)
 }
 
 // JSON implementation of cartesian serializer interface
-type JSONSerializer struct{}
+type JSONFormat struct{}
 
 // Decodes raw byte array to an array of points
-func (j *JSONSerializer) Decode(raw []byte) ([]services.Point, error) {
-	var points []services.Point
+func (j *JSONFormat) Decode(raw []byte) ([]models.Point, error) {
+	var points []models.Point
 
 	if err := json.Unmarshal(raw, &points); err != nil {
 		return nil, ErrSerialize(err.Error())
 	}
 	return points, nil
+}
+
+// Returns a new JSONFormat.
+func NewJSONFormat() CartesianSerializer {
+	return &JSONFormat{}
 }
