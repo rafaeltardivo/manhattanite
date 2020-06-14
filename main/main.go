10d495e946554f5b9870780d47c0a8feb1b4d7ef
Rafael Tardivo <rafaeldt.br@gmail.com>
2020-06-14 12:12:52 -0300

    Add main package

---
+++ main/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"sort"
+
+	"github.com/joho/godotenv"
+	"github.com/manhattanite/models"
+	"github.com/manhattanite/services"
+	"github.com/manhattanite/utils"
+)
+
+func init() {
+	godotenv.Load(".env")
+}
+
+func main() {
+	service, err := services.NewCartesian()
+	if err != nil {
+		// TODO log
+		os.Exit(1)
+	}
+
+	http.HandleFunc("/api/points", func(w http.ResponseWriter, r *http.Request) {
+
+		if err := utils.ValidateHTTPMethod(r); err != nil {
+			utils.JSONResponse(w, utils.ErrorResponse{Msg: err.Error()}, http.StatusForbidden)
+			return
+		}
+
+		params, err := utils.ValidateRequest(r)
+		if err != nil {
+			utils.JSONResponse(w, utils.ErrorResponse{Msg: err.Error()}, http.StatusBadRequest)
+			return
+		}
+
+		queriedPoint := models.Point{X: params.X, Y: params.Y}
+		points, err := service.FindPointsWithinDistance(queriedPoint, params.MaxManhattanDistance)
+		if err != nil {
+			utils.JSONResponse(w, utils.ErrorResponse{Msg: err.Error()}, http.StatusNotFound)
+			return
+		}
+		sort.Sort(models.PointDistanceSorter(points))
+		utils.JSONResponse(w, points, http.StatusOK)
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
