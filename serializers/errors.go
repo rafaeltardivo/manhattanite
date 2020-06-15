24833db8a642dd8e658680c253552a10a45323dc
Rafael Tardivo <rafaeldt.br@gmail.com>
2020-06-14 21:48:45 -0300

    Update exported objects comments

--- serializers/errors.go
+++ serializers/errors.go
@@ -2,7 +2,7 @@ package serializers
 
 import "fmt"
 
-// Error wrapper for file  errors.
+// ErrSerialize wrapper for file  errors.
 func ErrSerialize(msg string) error {
 	return fmt.Errorf("error serializing data: %s", msg)
 }
